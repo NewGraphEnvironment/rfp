bba2fdf7cbf900efebf62323138ea1cfcdd00405
almac2022 <al@newgraphenvironment.com>
2024-06-12 07:58:00 -0700

    improve docmentation

--- data-raw/rfp_xref_layers_custom.R
+++ data-raw/rfp_xref_layers_custom.R
@@ -1,5 +1,7 @@
 
-#' This is a table that cross references the names of the layers in GIS projects with metadata about where they come from and what they represent.
+#' This is a table that cross references the names of the layers in GIS projects with metadata about where they come
+#' from and what they represent. Layers include `rfp_tracking`, [bcfishpass](https://github.com/smnorris/bcfishpass) (bcfishpass schema and modelling input parameters)
+#' and [bcfishobs](https://github.com/smnorris/bcfishobs) output layers.
 #' @name rfp_xref_layers_custom
 #' @docType data
 #' @author Al Irvine \email{al@newgraphenvironment.com}
